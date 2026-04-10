1beb81f5965550181060eaedfc8b3b0a7086b9cb
Tommaso R. Marena <marena@cua.edu>
2026-04-10 06:21:10 -0400

    fix: suppress LightGBM feature names warning

--- qpos/disorder/ensemble.py
+++ qpos/disorder/ensemble.py
@@ -1,5 +1,6 @@
 import numpy as np
 import warnings
+import pandas as pd
 
 class DisorderNetV6:
     def __init__(self, config=None):
@@ -14,6 +15,7 @@ class DisorderNetV6:
             self.models_available = False
             
         self.is_fitted = False
+        self.feature_names_ = None
         
     def fit(self, sequences, labels):
         if not self.models_available:
@@ -24,8 +26,12 @@ class DisorderNetV6:
         X_list = [extract_features(seq) for seq in sequences]
         X = np.vstack(X_list)
         y = np.concatenate(labels)
-        self.lgbm.fit(X, y)
-        self.xgb.fit(X, y)
+        
+        self.feature_names_ = [f'feature_{i}' for i in range(X.shape[1])]
+        X_df = pd.DataFrame(X, columns=self.feature_names_)
+        
+        self.lgbm.fit(X_df, y)
+        self.xgb.fit(X_df, y)
         self.is_fitted = True
         
     def predict(self, sequence: str) -> np.ndarray:
@@ -34,8 +40,10 @@ class DisorderNetV6:
             
         from .features import extract_features
         X = extract_features(sequence)
-        p_lgbm = self.lgbm.predict_proba(X)[:, 1]
-        p_xgb = self.xgb.predict_proba(X)[:, 1]
+        
+        X_df = pd.DataFrame(X, columns=self.feature_names_)
+        p_lgbm = self.lgbm.predict_proba(X_df)[:, 1]
+        p_xgb = self.xgb.predict_proba(X_df)[:, 1]
         return (p_lgbm + p_xgb) / 2.0
         
     def _heuristic_predict(self, sequence: str) -> np.ndarray:
